900abc2a31dc219fc886821f22c4d44ca159f1ec
Roman Kumarbaev <kumarbaevrs@gmail.com>
2023-04-26 01:26:45 +0300

    Roman's commit 26-04-2023
     add sout "Privet"

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -31,4 +31,5 @@ public class ApplicationManager {
 
         wd.quit();
     }
+
 }
--- src/test/java/Roman_sTests.java
+++ src/test/java/Roman_sTests.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class Roman_sTests extends TestBase{
     @Test
     public void example(){
-
+        System.out.println("Privet");
     }
 
 }
